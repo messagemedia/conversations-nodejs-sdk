9669e448a5f5f8cccfbcc56b54e30a46bd8ed12e
Ibrahim <ibrahimtareq@live.com>
2018-06-04 14:13:32 +1000

    Added code examples and updated package.json

---
+++ examples/authorize.js
@@ -0,0 +1,11 @@
+const lib = require('../../lib');
+
+// Configuration parameters and credentials
+lib.Configuration.basicAuthUserName = "API_KEY"; // The username to use with basic authentication
+lib.Configuration.basicAuthPassword = "API_SECRET"; // The password to use with basic authentication
+
+var controller = lib.FacebookController;
+
+controller.getFacebookAuthorisationUrlUsingGET(function(error, response, context) {
+    console.log(response);
+});
